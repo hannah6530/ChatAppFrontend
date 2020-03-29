7be3588801ba9c54d043d8d46602c2d46f91583f
“hannah6530” <hannah6530@gmail.com>
2020-03-29 13:39:56 -0400

    test the fetch for the backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginPage from "./Components/LoginPage";
 
-const App = (props) => {
-    const initialState = useState({username: '', password: '', current_user_id: ''});
+const App = () => {
+    const initialState = useState({first_name: '', last_name: '', email_address: '', username: '', password: '', current_user_id: ''});
+
+    useEffect(() => {
+
+        fetch('http://localhost:8080/users', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(userData => {
+                console.log(userData)
+            })
+    });
 
   return (
       <React.Fragment>
-          <LoginPage/>
+          <LoginPage
+          />
       </React.Fragment>
   );
 };
+
+
+
 export default App;
--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 
 const LoginPage = (props) => {
 
+    const loginState = useState({username: '', password: '', current_user_id: ''});
+
+
+
     return(
         <React.Fragment>
             <h1>Ichat</h1>
-
             <div className="box">
                 <h2>Login</h2>
                 <form>
                     <div className="inputBox">
-                        <input type="text" name="username" required="" value="" />
+                        <input type="text" name="username" value={loginState[0].username}  onChange={event => loginState[1]({username: event.target.value})}/>
                             <label>Username</label>
                     </div>
                     <div className="inputBox">
-                        <input type="password" name="password" required="" value="" />
+                        <input type="text"  name="password" value={loginState[0].password} onChange={event => loginState[1]({password: event.target.value})}/>
                             <label>Password</label>
                     </div>
                     <input type="submit" name="submit" value="submit" />
